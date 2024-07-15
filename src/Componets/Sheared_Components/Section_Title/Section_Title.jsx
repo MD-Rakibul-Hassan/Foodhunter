769209952f666c1a822f43b58749bba029ad6e38
Rakibul-hassan <mdrakibulhassan2425@gmail.com>
2024-07-15 10:14:32 +0600

    Section title global component for this project is added

---
+++ src/Componets/Sheared_Components/Section_Title/Section_Title.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Section_Title = ({ title, subTitle }) => {
+	return (
+		<div className="my-5">
+			<div className="container w-full flex flex-col justify-center  mx-auto ">
+				<div className="flex items-center w-full flex-col">
+					<p className="font-semibold text-yellow-400">{subTitle ? subTitle : "--Subtitle here--"}</p>
+					<div className="bg-slate-500 h-[4px] w-[300px] my-2"></div>
+					<h1 className=" text-2xl lg:text-4x font-bold my-3">{title ? title : "Title here"}</h1>
+					<div className="bg-slate-500 h-[4px] w-[300px] my-2"></div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Section_Title;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,19 @@
+import Section_Title from "../../Componets/Sheared_Components/Section_Title/Section_Title";
 import Header from "./Header/Header";
 
 
 const Home = () => {
   return (
 		<div>
-          {/* Header com  */}
-          <Header />
+			{/* Header com  */}
+			<Header />
 			{/* Section Title com  */}
+			<div className="mt-10 lg:mt-20">
+				<Section_Title
+					title="ORDER ONLINE"
+					subTitle="---From 11:00am at 10:00pm---"
+				/>
+			</div>
 			{/* Menu Slider com  */}
 			{/* Section Title com  */}
 			{/* Overlay  */}
